fcbc507e892d8c3c76ccac0a9c12b4dbdd866ba2
valentina abramova <valentina@v.local>
2017-11-07 13:47:16 -0600

    nav bar and sort of burger menu

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Header />
+       <div>
+
+     
+         
+         </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
